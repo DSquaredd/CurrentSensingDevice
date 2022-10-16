9a7f41dae80aad48abb951bf3c5aabecd3fbcf32
Dyanette Arroyo <115327300+DSquaredd@users.noreply.github.com>
2022-10-16 14:32:14 -0400

    Add files via upload

---
+++ 1stProgram/config.h
@@ -0,0 +1,16 @@
+/*
+ * config.h
+ *
+ * Created: 8/15/2022 3:55:18 PM
+ *  Author: DDOzzy
+ */ 
+
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#define F_CPU     16000000UL
+
+
+
+#endif /* CONFIG_H_ */
